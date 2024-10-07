ee6ce7525f1a235eef32854773b0771bb08656cf
edwardo1239 <edwarsthat@gmail.com>
2024-10-07 17:10:10 -0500

    se cambio el directorio de las fotos de calidad v3

--- server/api/Proceso.js
+++ server/api/Proceso.js
@@ -675,7 +675,7 @@ class ProcesoRepository {
     }
     static async ingresar_foto_calidad(req, user) {
         const { foto, fotoName, _id } = req
-        const fotosPath = path(__dirname, "fotos_frutas");
+        const fotosPath = path.join(__dirname, "fotos_frutas");
         const base64Data = foto.replace(/^data:image\/\w+;base64,/, "");
         const fotoPath = fotosPath + _id + "_" + fotoName + ".png";
         console.log(fotosPath)
